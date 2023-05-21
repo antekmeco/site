bf9d98a3a256ed75edd4810d22f4140446537035
Antoni M <n1k000@protonmail.com>
2023-05-21 16:58:03 +0200

    aahh

--- pages/stuff.vue
+++ pages/stuff.vue
@@ -5,7 +5,7 @@
       class="mr-3 ml-3 mt-3 mb-3"
     >
       <div class="rounded-lg border-nord7 border-solid border-4">
-        <a href="/">
+        <a href="https://github.com/antekmeco/site/tree/main">
           <p class="mr-3 ml-3 mt-3 mb-3">This site</p>
 
           <h6 class="mr-3 ml-3 mt-3 mb-3">idk i was bored af</h6>
